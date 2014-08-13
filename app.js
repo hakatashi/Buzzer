35b0567ee4f401ece6e761468aedc4bb3a51e15f
hakatashi <hakatasiloving@gmail.com>
2014-08-13 15:35:48 +0900

    Enable Twitter authentication

--- app.js
+++ app.js
@@ -4,8 +4,37 @@ var morgan = require('morgan');
 var bodyParser = require('body-parser');
 var flash = require('connect-flash');
 var serveStatic = require('serve-static');
+var passport = require('passport');
+var passportTwitter = require('passport-twitter');
 
 var config = require('./config');
+
+/***** Setup passport *****/
+
+passport.serializeUser(function(user, done) {
+	done(null, user);
+});
+
+passport.deserializeUser(function(obj, done) {
+	done(null, obj);
+});
+
+passport.use(new passportTwitter.Strategy({
+		consumerKey: config.consumerKey,
+		consumerSecret: config.consumerSecret,
+		callbackURL: "http://127.0.0.1:8888/auth/twitter/callback"
+	},
+	function(token, tokenSecret, profile, done) {
+		process.nextTick(function () {
+			return done(null, profile);
+		});
+	}
+));
+
+var authenticatedOnly = function (req, res, next) {
+	if (req.isAuthenticated()) { return next(); }
+	res.redirect('/login')
+};
 
 /***** Setup express.js *****/
 
@@ -22,8 +51,8 @@ app.use(session({
 	saveUninitialized: true
 }));
 app.use(flash());
-// app.use(passport.initialize());
-// app.use(passport.session());
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(serveStatic('assets', {
 	index: false,
 	redirect: false
@@ -31,8 +60,27 @@ app.use(serveStatic('assets', {
 
 /** routes **/
 
-app.get('/', function (req, res) {
-	res.render('index');
+app.get('/', authenticatedOnly, function (req, res) {
+	res.render('index', {user: req.user});
+});
+
+app.get('/account', authenticatedOnly, function(req, res){
+	res.render('account', {user: req.user});
+});
+
+app.get('/login', function(req, res){
+	res.render('login', {user: req.user});
+});
+
+app.get('/auth/twitter', passport.authenticate('twitter'), function (req, res) {
+	// The request will be redirected to Twitter for authentication, so this
+	// function will not be called.
+});
+
+app.get('/auth/twitter/callback', passport.authenticate('twitter', {
+	failureRedirect: '/login'
+}), function (req, res) {
+	res.redirect('/');
 });
 
 var server = app.listen(config.port, function () {
