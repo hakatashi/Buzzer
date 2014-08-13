87a61a9bc0ca4315bc5e0523085173f9ae034045
hakatashi <hakatasiloving@gmail.com>
2014-08-13 15:37:36 +0900

    Add Twitter application settings

--- config.default.js
+++ config.default.js
@@ -3,4 +3,8 @@ var config = {};
 config.port = 8888;
 config.sessionSecret = 'secret';
 
+// Twitter application settings
+config.consumerKey = 'xxxxxxxxxxxxxxxxxxxxxxxxx';
+config.consumerSecret = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx';
+
 module.exports = config;
